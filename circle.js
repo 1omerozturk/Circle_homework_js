0bb5e7842b43779d9078ecc2d6dfbe820c2f4f39
Ömer  ÖZTÜRK <omer.ozturk.or.ok@gmail.com>
2022-10-07 13:51:03 +0300

    Patika.dev nodejs Circle homework

---
+++ circle.js
@@ -0,0 +1,13 @@
+
+   function circleArea(r){
+        console.log(`radius: 5 to be circle Area is ${Math.PI*r**2}`);
+    }
+    function circleCircumference(r){
+       return console.log(`radius: 5 to be Circle circum ference is ${Math.PI*r*2} `)
+    }
+    
+
+module.exports={
+    circleArea,
+    circleCircumference
+}
